e63739da2503fa222288b5c3b45a48f998f4328a
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-20 14:16:48 -0400

    Created variables

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class NumberGuessing {
-//I have corrected the bad code by putting it in comments
+
+    public static void main(String[]args) {
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+   //Declare
+
+        int pickedNumber;
+        pickedNumber = (int)(Math.random()*100);
+
+   //Code
+
+        System.out.println("Pick a number");
+        int guess = keyboard.nextInt();
+        if(guess>pickedNumber){
+            System.out.println("Too high, guess again");
+            guess = keyboard.nextInt();
+        }
+        else if(guess<=pickedNumber) {
+            System.out.println("Too low, guess again");
+            guess = keyboard.nextInt();
+        }
+        else if(guess==pickedNumber) {
+            System.out.println("Good job! The number is " + guess);
+        }
+
+
+
+
+
+
+    }
 }
+
