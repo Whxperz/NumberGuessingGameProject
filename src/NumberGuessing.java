126914d316aa42a257c31f342d8d1bffe3b874c4
Whxperz <42781392+Whxperz@users.noreply.github.com>
2018-10-12 20:27:22 -0400

    Additional Features

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NumberGuessing {
+public class NumberGuessingProject {
 
     public static void main(String[]args) {
 
@@ -8,24 +8,45 @@ public class NumberGuessing {
         keyboard = new Scanner(System.in);
 
         //Declare
-
+        String cont_game;
         int pickedNumber;
-        pickedNumber = (int)(Math.random()*100);
+        String high_low;
+        int numberoftry;
+
+        do{
+
+            pickedNumber = (int) (Math.random() * 100);
 
         //Code
-        System.out.println("Pick a number");
+        //  System.out.println(pickedNumber);
+            System.out.println("Pick a number");
+
         int guess;
+        high_low ="";
+        numberoftry = 0;
         do {
             guess = keyboard.nextInt();
-            if (guess > pickedNumber) {
-                System.out.println("Too high, guess again");
-            } else if (guess < pickedNumber) {
-                System.out.println("Too low, guess again");
-            } else if (guess == pickedNumber) {
+            numberoftry++;
+           // System.out.println(numberoftry);
+            if (guess > pickedNumber)
+                high_low = "Too high";
+            else if (guess < pickedNumber)
+                high_low = "Too low";
+            else if (guess == pickedNumber) {
                 System.out.println("Good job! The number is " + guess + "!");
+                break;
             }
-        }  while (guess != pickedNumber);
-
+           if (numberoftry == 5) {
+                System.out.println(high_low);
+                System.out.println("You used all your tries");
+                break;
+            }
+            else
+                System.out.println(high_low +", guess again");
+        } while (guess != pickedNumber);
+        System.out.println("Do you want to play again Yes/No?");
+        cont_game = keyboard.next();
+    } while (cont_game.equalsIgnoreCase("Yes"));
+       System.out.println("Thank you for playing! Have a nice day!");
     }
 }
-
